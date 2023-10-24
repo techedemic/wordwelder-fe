9592b42e5620198a9003e06c7e51ae9ef56adb4d
Hendri <108517230+hendri-schoeman@users.noreply.github.com>
2023-10-24 15:29:24 +0200

    Add sentencelist component - empty - want to add tailwindcss first

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SentenceList from "./components/SentenceList";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <SentenceList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SentenceList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SentenceList = (props: Props) => {
+  return <div>List of sentences</div>;
+};
+
+export default SentenceList;
